3fb42d3a4339222ad6a7de680582c73756740b6e
ngvanhao1801 <ngohao1801@gmail.com>
2023-10-24 23:10:22 +0700

    first commit

--- src/main/java/hunre/it/lecturer/controller/LecturerController.java
+++ src/main/java/hunre/it/lecturer/controller/LecturerController.java
@@ -17,84 +17,84 @@ import java.util.List;
 @Controller
 public class LecturerController {
 
-	@Autowired
-	private LecturerService service;
+  @Autowired
+  private LecturerService service;
 
-	@Autowired
-	private MyLecturerListService myLecturerListService;
+  @Autowired
+  private MyLecturerListService myLecturerListService;
 
-	@GetMapping("/")
-	public String home() {
-		return "home";
-	}
+  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
 
-	@GetMapping("/lecturer_register")
-	public String lecturerRegister() {
-		return "lecturer-register";
-	}
+  @GetMapping("/lecturer_register")
+  public String lecturerRegister() {
+    return "lecturer-register";
+  }
 
-	@GetMapping("/available_lecturers")
-	public ModelAndView getAllLecturer() {
-		List<Lecturer> list = service.getAllLecturer();
+  @GetMapping("/available_lecturers")
+  public ModelAndView getAllLecturer() {
+    List<Lecturer> list = service.getAllLecturer();
 
-		Collections.sort(list, Comparator.comparing(Lecturer::getHeSoLuong));
+    Collections.sort(list, Comparator.comparing(Lecturer::getHeSoLuong));
 
-		// Bổ sung cột tình trạng
-		for (Lecturer lecturer : list) {
-			if (lecturer.getHeSoLuong() >= 5) {
-				lecturer.setTinhTrang("Cao");
-			} else if(lecturer.getHeSoLuong() >= 3 && lecturer.getHeSoLuong() <5) {
-				lecturer.setTinhTrang("Trung Bình");
-			} else {
-				lecturer.setTinhTrang("Thấp");
-			}
-		}
+    // Bổ sung cột tình trạng
+    for (Lecturer lecturer : list) {
+      if (lecturer.getHeSoLuong() >= 5) {
+        lecturer.setTinhTrang("Cao");
+      } else if (lecturer.getHeSoLuong() >= 3 && lecturer.getHeSoLuong() < 5) {
+        lecturer.setTinhTrang("Trung Bình");
+      } else {
+        lecturer.setTinhTrang("Thấp");
+      }
+    }
 
-		return new ModelAndView("lecturer-list", "lecturer", list);
-	}
+    return new ModelAndView("lecturer-list", "lecturer", list);
+  }
 
-	@PostMapping("/save")
-	public String addLecturer(@ModelAttribute Lecturer l) {
-		service.save(l);
-		return "redirect:/available_lecturers";
-	}
+  @PostMapping("/save")
+  public String addLecturer(@ModelAttribute Lecturer l) {
+    service.save(l);
+    return "redirect:/available_lecturers";
+  }
 
-	@GetMapping("/my_lecturers")
-	public String getMyLecturers(Model model) {
-		List<MyLecturerList> list = myLecturerListService.getAllMyLecturers();
-		model.addAttribute("lecturer", list);
-		return "my-lecturer";
-	}
+  @GetMapping("/my_lecturers")
+  public String getMyLecturers(Model model) {
+    List<MyLecturerList> list = myLecturerListService.getAllMyLecturers();
+    model.addAttribute("lecturer", list);
+    return "my-lecturer";
+  }
 
-	@RequestMapping("/mylist/{id}")
-	public String getMyList(@PathVariable("id") int id) {
-		Lecturer l = service.getLecturerById(id);
-		MyLecturerList ml = new
-				MyLecturerList(l.getId(), l.getMaGv(), l.getHoTen(), l.getTenBoMon(), l.getHeSoLuong());
-		myLecturerListService.saveMyLecturers(ml);
-		return "redirect:/my_lecturers";
-	}
+  @RequestMapping("/mylist/{id}")
+  public String getMyList(@PathVariable("id") int id) {
+    Lecturer l = service.getLecturerById(id);
+    MyLecturerList ml = new
+        MyLecturerList(l.getId(), l.getMaGv(), l.getHoTen(), l.getTenBoMon(), l.getHeSoLuong());
+    myLecturerListService.saveMyLecturers(ml);
+    return "redirect:/my_lecturers";
+  }
 
-	@RequestMapping("/editLecturer/{id}")
-	public String editLecturer(@PathVariable("id") int id, Model model) {
-		Lecturer l = service.getLecturerById(id);
-		model.addAttribute("lecturer", l);
-		return "lecturer-edit";
-	}
+  @RequestMapping("/editLecturer/{id}")
+  public String editLecturer(@PathVariable("id") int id, Model model) {
+    Lecturer l = service.getLecturerById(id);
+    model.addAttribute("lecturer", l);
+    return "lecturer-edit";
+  }
 
-	@RequestMapping("/deleteLecturer/{id}")
-	public String deleteLecturer(@PathVariable("id") int id) {
-		service.deleteById(id);
-		return "redirect:/available_lecturers";
-	}
+  @RequestMapping("/deleteLecturer/{id}")
+  public String deleteLecturer(@PathVariable("id") int id) {
+    service.deleteById(id);
+    return "redirect:/available_lecturers";
+  }
 
-	@GetMapping("/search")
-	public String searchLecturer(@RequestParam("tenBoMon") String tenBoMon,
-	                             @RequestParam("heSoLuong") int heSoLuong,
-	                             Model theModel) {
-		List<Lecturer> lecturers = service.searchLecturer(tenBoMon, heSoLuong);
-		theModel.addAttribute("lecturer", lecturers);
-		return "search-lecturer";
-	}
+  @GetMapping("/search")
+  public String searchLecturer(@RequestParam("tenBoMon") String tenBoMon,
+                               @RequestParam("heSoLuong") int heSoLuong,
+                               Model theModel) {
+    List<Lecturer> lecturers = service.searchLecturer(tenBoMon, heSoLuong);
+    theModel.addAttribute("lecturer", lecturers);
+    return "search-lecturer";
+  }
 
 }
--- src/main/java/hunre/it/lecturer/entity/Lecturer.java
+++ src/main/java/hunre/it/lecturer/entity/Lecturer.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 public class Lecturer {
 
   @Id
-  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
   private String maGv;
